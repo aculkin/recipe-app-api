46a9f3e09dff161c30064c0b9e33647f7f624f1e
Andrew Culkin <andrewsculkin@gmail.com>
2021-10-14 10:58:13 -0400

    Fixing typo issue causing tests to fail

--- app/recipe/tests/test_tags_api.py
+++ app/recipe/tests/test_tags_api.py
@@ -7,14 +7,14 @@ from rest_framework.test import APIClient
 
 from core.models import Tag
 
-from recepie.serializers import TagSerializer
+from recipe.serializers import TagSerializer
 
 
 TAGS_URL = reverse('recipe:tag-list')
 
 
 def create_test_user(email='test@email.com', password='testpass'):
-    return get_user_model().objects.create_user()
+    return get_user_model().objects.create_user(email, password)
 
 
 class PublicTagsAPITests(TestCase):
@@ -57,8 +57,25 @@ class PrivateTagsAPITests(TestCase):
         Tag.objects.create(user=user2, name='Fruity')
         tag = Tag.objects.create(user=self.user, name='Comfort Foot')
 
-        res.self.client.get(TAGS_URL)
+        res = self.client.get(TAGS_URL)
 
         self.assertEqual(res.status_code, status.HTTP_200_OK)
         self.assertEqual(len(res.data), 1)
         self.assertEqual(res.data[0]['name'], tag.name)
+    #
+    # def test_create_tag_successful(self):
+    #     payload = {'name': 'Test tag'}
+    #     self.client.post(TAGS_URL, payload)
+    #
+    #     exists = Tag.objects.filter(
+    #         user=self.user,
+    #         name=payload['name']
+    #     ).exists()
+    #     self.assertTrue(exists)
+    #
+    # def test_create_tag_invalid(self):
+    #     """Test creating a new tag with an invalid payload"""
+    #     payload = {'name': ''}
+    #     res.self.client.post(TAGS_URL, payload)
+    #
+    #     self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
